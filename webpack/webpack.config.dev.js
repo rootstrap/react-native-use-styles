33fec9c782ddf7a57ce659539dc88acfdccf3546
Agustín <agustin@rootstrap.com>
2020-08-29 12:16:41 -0300

    Initial commit

---
+++ webpack/webpack.config.dev.js
@@ -0,0 +1,47 @@
+const Path = require('path');
+const Webpack = require('webpack');
+const { merge } = require('webpack-merge');
+const common = require('./webpack.common.js');
+
+module.exports = merge(common, {
+  mode: 'development',
+  devtool: 'cheap-eval-source-map',
+  output: {
+    chunkFilename: 'js/[name].chunk.js'
+  },
+  devServer: {
+    inline: true,
+    hot: true
+  },
+  plugins: [
+    new Webpack.DefinePlugin({
+      'process.env.NODE_ENV': JSON.stringify('development')
+    })
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        include: Path.resolve(__dirname, '../src'),
+        enforce: 'pre',
+        loader: 'eslint-loader',
+        options: {
+          emitWarning: true
+        }
+      },
+      {
+        test: /\.html$/i,
+        loader: 'html-loader',
+      },
+      {
+        test: /\.js$/,
+        include: Path.resolve(__dirname, '../src'),
+        loader: 'babel-loader'
+      },
+      {
+        test: /\.s?css$/i,
+        use: ['style-loader', 'css-loader?sourceMap=true', 'sass-loader']
+      }
+    ]
+  }
+});
