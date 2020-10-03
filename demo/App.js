5f4c993843c2bb7b6ad57f551732c261d077aa1f
lmajhi <lmajhi.vssut@gmail.com>
2020-10-03 23:36:20 +0530

    Lint fixes.

--- demo/App.js
+++ demo/App.js
@@ -1,8 +1,8 @@
-import './styles/global';
-import React from 'react';
-import { View, Text } from 'react-native';
+import "./styles/global";
+import React from "react";
+import { View, Text } from "react-native";
 
-import useStyles from './styles/namespaced';
+import useStyles from "./styles/namespaced";
 
 export default function App() {
   const s = useStyles();
