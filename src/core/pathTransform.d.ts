fc3697e7fba0ad5152f24a3fe8d67ce1ba0e4ace
Vitor Freitas <vitor_freitas_b@hotmail.com>
2020-10-08 23:40:59 -0300

    feat(types): move types to index.d.ts

--- src/core/pathTransform.d.ts
+++
@@ -1,9 +0,0 @@
-export let separator: string;
-export function setSeparator(sp: string): void;
-
-declare function pathTransform(
-  path: string,
-  getConstant: Function
-): object;
-
-export default pathTransform;
--- src/index.d.ts
+++ src/index.d.ts
@@ -1,7 +1,19 @@
-export { setSeparator } from "./core/pathTransform";
-export {
-  Styles,
-  GlobalStyles,
-  GlobalUse,
-  useGlobalStyles
-} from "./core/stylesManager";
+type StyleObject = {
+  [key: string]: unknown;
+};
+
+export function setSeparator(sp: string): void;
+
+export function GlobalUse(path: string, namespace: string): StyleObject;
+
+export function useGlobalStyles(nameSpace: string): (strings: string[], expressions: string[]) => StyleObject;
+
+export function GlobalStyles(
+  definition: StyleObject,
+  namespace: string
+): void;
+
+export function Styles(definition: StyleObject, namespace: string): {
+  (): () => typeof useGlobalStyles;
+  namespace: string;
+};
