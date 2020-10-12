49d5dadb168363ea6daded23721272a7f0a6bf1c
Chris Frewin <frewin.christopher@gmail.com>
2020-10-12 09:16:21 +0200

    fixed console.warn output coming from other tests

--- tests/core/globalCache.spec.js
+++ tests/core/globalCache.spec.js
@@ -4,9 +4,12 @@ import {
   clearCache
 } from "../../src/core/globalCache";
 
+const consoleSpy = jest.spyOn(console, 'warn').mockImplementation()
+
 describe("utils", () => {
   beforeEach(() => {
     clearCache();
+    consoleSpy.mockClear();
   });
 
   it("setInCache sets in global cache properly", async () => {
@@ -22,9 +25,13 @@ describe("utils", () => {
   it("getFromCache gets in global cache when namespaced", async () => {
     setInCache({ style: true });
     expect(getFromCache("style", "namespace")).toBe(true);
+    expect(console.warn).toBeCalledTimes(1);
+    expect(console.warn).toHaveBeenLastCalledWith('Non-Existent-Namespace: The following namespace does not exist or has not been imported: "namespace". You are seeing this warning because you are in development mode. In a production build there will be no warning and these styles will be ignored.');
   });
 
   it("getFromCache doesn't get from cache", async () => {
     expect(getFromCache("style", "namespace")).toBe(undefined);
+    expect(console.warn).toBeCalledTimes(1);
+    expect(console.warn).toHaveBeenLastCalledWith('Non-Existent-Namespace: The following namespace does not exist or has not been imported: "namespace". You are seeing this warning because you are in development mode. In a production build there will be no warning and these styles will be ignored.');
   });
 });
