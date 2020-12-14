a46df33ea348e6bc1f3ccf519e1ce5d68cd2b0c1
agustito37 <agustin_prieto37@outlook.com>
2020-12-14 13:25:26 -0300

    Remove typescript module conflict

---
+++ index.d.ts
@@ -0,0 +1,33 @@
+// based on src/index.js
+type StyleObject = {
+  [key: string]: unknown;
+};
+
+type UniqueSpace = symbol | string;
+
+type Computed = (dependencies: Array<any>) => StyleObject;
+
+type TagStyles = (strings: string[], expressions: string[]) => StyleObject;
+
+type UseStyles = {
+  (): (dependencies: Array<any>) => TagStyles;
+  namespace: UniqueSpace;
+};
+
+export function useGlobalStyles(
+  namespace: UniqueSpace,
+  dependencies: Array<any>,
+): TagStyles;
+
+export function GlobalUse(rawStyle: string, namespace: UniqueSpace): Computed;
+
+export function GlobalStyles(definition: StyleObject, namespace: UniqueSpace): void;
+
+export function Styles(
+  definition: StyleObject,
+  namespace: UniqueSpace,
+): UseStyles;
+
+export function getConstant(name: string, namespace: UseStyles | UniqueSpace): any;
+
+export function setSeparator(newSeparator: string): void;
