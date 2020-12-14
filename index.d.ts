3cfd8752e6d01fa7b4e5fcb6f198032b078fb128
agustito37 <agustin_prieto37@outlook.com>
2020-12-14 13:44:07 -0300

    Add conditional arguments

--- index.d.ts
+++ index.d.ts
@@ -5,29 +5,29 @@ type StyleObject = {
 
 type UniqueSpace = symbol | string;
 
-type Computed = (dependencies: Array<any>) => StyleObject;
+type Computed = (dependencies?: Array<any>) => StyleObject;
 
 type TagStyles = (strings: string[], expressions: string[]) => StyleObject;
 
 type UseStyles = {
-  (): (dependencies: Array<any>) => TagStyles;
+  (): (dependencies?: Array<any>) => TagStyles;
   namespace: UniqueSpace;
 };
 
 export function useGlobalStyles(
-  namespace: UniqueSpace,
-  dependencies: Array<any>,
+  namespace?: UniqueSpace,
+  dependencies?: Array<any>,
 ): TagStyles;
 
-export function GlobalUse(rawStyle: string, namespace: UniqueSpace): Computed;
+export function GlobalUse(rawStyle: string, namespace?: UniqueSpace): Computed;
 
-export function GlobalStyles(definition: StyleObject, namespace: UniqueSpace): void;
+export function GlobalStyles(definition: StyleObject, namespace?: UniqueSpace): void;
 
 export function Styles(
   definition: StyleObject,
-  namespace: UniqueSpace,
+  namespace?: UniqueSpace,
 ): UseStyles;
 
-export function getConstant(name: string, namespace: UseStyles | UniqueSpace): any;
+export function getConstant(name: string, namespace?: UseStyles | UniqueSpace): any;
 
 export function setSeparator(newSeparator: string): void;
